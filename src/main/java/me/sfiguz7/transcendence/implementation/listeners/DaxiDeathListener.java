892a04e91dfd1d836b49bfea3dd97d09ad52e2b0
Venom Lightning <dentatar95@gmail.com>
2024-11-20 01:44:01 +0300

    Revert 13332f6c4ec17eb746aecefe9aa3b78abe66a699 not working as intended

--- src/main/java/me/sfiguz7/transcendence/implementation/listeners/DaxiDeathListener.java
+++ src/main/java/me/sfiguz7/transcendence/implementation/listeners/DaxiDeathListener.java
@@ -5,15 +5,15 @@ import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import me.sfiguz7.transcendence.TranscEndence;
 import me.sfiguz7.transcendence.implementation.items.items.Daxi;
 import me.sfiguz7.transcendence.lists.TEItems;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -23,6 +23,7 @@ import static org.bukkit.event.EventPriority.LOWEST;
 public class DaxiDeathListener implements Listener {
 
     public static final NamespacedKey TINKER_PROTECTION = new NamespacedKey(TranscEndence.getInstance(), "tinker");
+    private static final Log log = LogFactory.getLog(DaxiDeathListener.class);
 
     public DaxiDeathListener(JavaPlugin plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
@@ -43,13 +44,8 @@ public class DaxiDeathListener implements Listener {
         UUID uuid = p.getUniqueId();
         Map<UUID, Set<Daxi.Type>> activePlayers = TranscEndence.getRegistry().getDaxiEffectPlayers();
         if (activePlayers.get(uuid) != null) {
-            // Keep inventory plugins may set drops to false, so we can't just add to e.getDrops().
-            // To work around this, we try to add the stable blocks to the player's inventory, and if that fails
-            // we drop them directly.
-            int howMany = activePlayers.get(uuid).size();
-            HashMap<Integer, ItemStack> notFitting = p.getInventory().addItem(new CustomItemStack(TEItems.STABLE_BLOCK, 8*howMany));
-            if (!notFitting.isEmpty()) {
-                p.getWorld().dropItem(p.getLocation(), notFitting.get(0));
+            for (Daxi.Type type : activePlayers.get(uuid)) {
+                e.getDrops().add(new CustomItemStack(TEItems.STABLE_BLOCK, 8));
             }
             activePlayers.remove(uuid);
         }
